cc9bdb86f9b7ccf9da0b323c207ac66b311e3bb1
HuXin0817 <202219120810@stu.cdut.edu.cn>
2025-02-17 18:49:30 +0800

    fmt Mock_test.go

--- src/mock/Mock_test.go
+++ src/mock/Mock_test.go
@@ -1,9 +1,5 @@
 package mock
 
-import (
-	"testing"
-)
+import "testing"
 
-func BenchmarkL4(b *testing.B) {
-	b.Log(RunAILocalMParallel("L4", "L4", 1, 1))
-}
+func BenchmarkL4(b *testing.B) { b.Log(RunAILocalMParallel("L4", "L4", 1, 1)) }
