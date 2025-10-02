6cf7ef4c188d10b68471a3fd3e46ba4da623368e
HuXin0817 <202219120810@stu.cdut.edu.cn>
2025-10-02 20:42:59 +0800

    Refactor RandomChoice function: rename parameter from 'vec' to 'data' for clarity and improve code readability.

--- src/defs.h
+++ src/defs.h
@@ -8,11 +8,11 @@ static constexpr int BoardSize = 6;
 
 template <typename T>
 [[nodiscard]] const T&
-RandomChoice(const std::span<T>& vec) {
+RandomChoice(const std::span<T>& data) {
   assert(!vec.empty());
 
-  static thread_local std::mt19937 rng(std::random_device{}());
-  std::uniform_int_distribution<size_t> dist(0, vec.size() - 1);
+  thread_local std::mt19937 rng(std::random_device{}());
+  std::uniform_int_distribution<size_t> dist(0, data.size() - 1);
 
-  return vec[dist(rng)];
+  return data[dist(rng)];
 }
